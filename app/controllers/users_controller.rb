bee2fc16c1b43fdab1c0684dbd7798890d246f68
github username <github email address>
2019-12-20 13:00:46 +0400

    fill out post '/login' and create get '/users:id' route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
 
     post '/login' do #finds and authenticates a user and logs them in/creates a session
         @user = User.find_by(email: params[:email])
-        if @user && @user.authenticate(params[:password])
-        
+        if @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect "/users/#{@user.id}" #redirect to user's home/"show" page
         else 
             #tell user they entered invalid credentials and redirect to login page
             redirect '/login'
@@ -16,5 +17,10 @@ class UsersController < ApplicationController
 
     get '/signup' do #render form for user to sign up
         erb :signup
+    end
+
+    get '/users/:id' do
+        "This will be the user's home/show page"
+        
     end
 end
