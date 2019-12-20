826d60adedac10eec3e46546b5ef9faad5574e76
github username <github email address>
2019-12-20 14:40:59 +0400

    stub out get /goals, get /goals/new, post '/goals, and get /goals/:id routes

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,4 +1,24 @@
 class GoalsController < ApplicationController
-    
 
+    get '/goals' do #index page for all goals
+        erb :index
+    end
+    
+    
+    get '/goals/new' do #render a form to create a new goal
+        erb :'goals/new'
+    end
+
+    post '/goals' do #creates a new goal and redirects to that goal's show page
+        if !(params.has_value?(""))
+            @goal = Goal.create(params)
+            redirect "/goals/#{@goal.id}"
+        else 
+            Invalid entry. Try again
+        end
+    end
+
+    get '/goals/:id' do #show page for single goal
+        erb :show
+    end
 end
