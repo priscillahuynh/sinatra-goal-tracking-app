cc61a972040a20cc5de332b8761a4e912e16ad9e
github username <github email address>
2019-12-20 12:36:36 +0400

    add 2 routes to users controller and create login.erb view file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,10 @@
 class UsersController < ApplicationController
-    
+
+    get '/login' do #render login form
+        erb :login
+    end
+
+    get '/signup' do 
+        erb :signup
+    end
 end
